7efaffe46feebc8877cc2a6c7c1f6f3d1560c9a1
Matthias <mtthsfrdrch@gmail.com>
2016-07-07 20:50:50 +0200

    show speakers fragment

--- app/src/main/java/de/devfest/screens/main/MainActivity.java
+++ app/src/main/java/de/devfest/screens/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +18,7 @@ import de.devfest.R;
 import de.devfest.databinding.ActivityMainBinding;
 import de.devfest.injection.ApplicationComponent;
 import de.devfest.mvpbase.BaseActivity;
+import de.devfest.screens.speakers.SpeakersFragment;
 
 public class MainActivity extends BaseActivity<MainView, MainPresenter> implements MainView,
         OnNavigationItemSelectedListener {
@@ -53,16 +56,14 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter> implemen
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         switch (id) {
             case R.id.nav_schedule:
                 break;
             case R.id.nav_sessions:
                 break;
             case R.id.nav_speakers:
-                // TODO: load SpeakersFragment
+                showFragment(new SpeakersFragment());
                 break;
             case R.id.nav_social:
                 break;
@@ -76,5 +77,14 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter> implemen
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.content_container) == null) {
+            fragmentManager.beginTransaction().add(R.id.content_container, fragment).commitNow();
+        } else {
+            fragmentManager.beginTransaction().replace(R.id.content_container, fragment).commitNow();
+        }
     }
 }
--- app/src/main/java/de/devfest/screens/speakers/SpeakersFragment.java
+++ app/src/main/java/de/devfest/screens/speakers/SpeakersFragment.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,8 +35,10 @@ public class SpeakersFragment extends BaseFragment<SpeakersView, SpeakersPresent
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_speakers, container, false);
+        binding.listSpeakers.setLayoutManager(new GridLayoutManager(getContext(), 2));
         adapter = new SpeakerAdapter();
         binding.listSpeakers.setAdapter(adapter);
+
         return binding.getRoot();
     }
 
