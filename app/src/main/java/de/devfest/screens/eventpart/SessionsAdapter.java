76ac1a96497e8f6ccd77fba6caac3324a63536e3
Matthias <mtthsfrdrch@gmail.com>
2016-10-03 16:43:23 +0200

    use SortedList for sessions

--- app/src/main/java/de/devfest/screens/eventpart/SessionsAdapter.java
+++ app/src/main/java/de/devfest/screens/eventpart/SessionsAdapter.java
@@ -2,16 +2,15 @@ package de.devfest.screens.eventpart;
 
 import android.databinding.DataBindingUtil;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.util.SortedList;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.util.SortedListAdapterCallback;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 
 import org.threeten.bp.format.DateTimeFormatter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import de.devfest.R;
 import de.devfest.databinding.ItemSessionBinding;
@@ -21,11 +20,12 @@ import de.devfest.ui.UiUtils;
 
 public class SessionsAdapter extends RecyclerView.Adapter<SessionsAdapter.SessionViewHolder> {
 
-    private List<Session> sessions = new ArrayList<>(0);
-    private DateTimeFormatter sessionStartFormat;
+    private final SortedList<Session> sessions;
+    private final DateTimeFormatter sessionStartFormat;
 
     public SessionsAdapter() {
         sessionStartFormat = UiUtils.getSessionStartFormat();
+        sessions = new SortedList<>(Session.class, new SessionsListUpdate(this));
     }
 
     @Override
@@ -74,6 +74,28 @@ public class SessionsAdapter extends RecyclerView.Adapter<SessionsAdapter.Sessio
         public SessionViewHolder(ItemSessionBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+        }
+    }
+
+    private class SessionsListUpdate extends SortedListAdapterCallback<Session> {
+
+        public SessionsListUpdate(RecyclerView.Adapter adapter) {
+            super(adapter);
+        }
+
+        @Override
+        public int compare(Session session1, Session session2) {
+            return session1.startTime.compareTo(session2.startTime);
+        }
+
+        @Override
+        public boolean areContentsTheSame(Session oldItem, Session newItem) {
+            return oldItem.id.equals(newItem.id);
+        }
+
+        @Override
+        public boolean areItemsTheSame(Session item1, Session item2) {
+            return item1.id.equals(item2.id);
         }
     }
 }
